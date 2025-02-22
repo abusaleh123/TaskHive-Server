f0451babe2fd2ad4c66ea5eade1b58e569bfd7a5
abusaleh123 <abusalehnoor4103@gmail.com>
2025-02-22 15:27:58 +0600

    Added Link

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173",
+  cors: { origin: "https://taskhive-server.onrender.com",
      methods: ["GET", "POST"],
    },
 });
